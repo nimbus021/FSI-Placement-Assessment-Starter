6c35c3ab2a42334d0a3e9a8c9eaadcb7d5031c46
Nghia Nguyen <aihgn224@gmail.com>
2021-12-07 19:54:24 -0800

    Completed Assignment

--- assets/script.js
+++ assets/script.js
@@ -13,63 +13,82 @@ let sugar = 0   // Sugar Sprinkle
 document.getElementById('credit').textContent = `Created by ${yourName}`
 
 
-
-    // TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
-
-
 // TODO: Hook up event listeners for the rest of the buttons
 
-let quantity = 0;
-const quantityDown= document.querySelector('#quanity-down')
-const quanityUp= document.querySelector('#quanity-up')
+
+
+console.log('JavaScript code has loaded')
+// TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
 
 document.getElementById('add-gb').addEventListener('click', function() {
-    console.log('Gingerbread + button was clicked!')
+    console.log('Gingerbread' + ' '+ 'button was clicked!')
     let plusButton =document.querySelector('.add-gb');
     
-        quantity ++;
-        document.getElementById('qty-gb').textContent = gb
-        updateQuantity(`Quantity: ${quantity}`)
-       
+        gb += 1;
+        document.getElementById('qty-gb').textContent = gb;
+        document.getElementById('qty-total').textContent= gb + cc+ sugar;
 })
+
+//code for gb - 
 document.getElementById('minus-gb').addEventListener('click', function() {
-    console.log('Gingerbread + minus button was clicked!')
-    let minusButton =document.querySelector('.minus-gb')
-    if(quantity > 0) {
-    quantity -= 1;}
-        document.getElementById('qty-gb').textContent = gb
-        updateQuantity(`Quantity: ${quantity}`)
+    console.log('Gingerbread' + ' ' +  'minus button was clicked!')
+  
+    if(gb > 0) {
+    gb -= 1;
+    }
+        document.getElementById('qty-gb').textContent = gb;
+        document.getElementById('qty-total').textContent= gb + cc+ sugar;
+       
+        
 })
 
-
+//code for cc +
 document.getElementById('add-cc').addEventListener('click', function(){
-    console.log('Chocolate Chip + button was clicked')
+    console.log('Chocolate Chip' + ' ' + 'button was clicked')
     let plusButton =document.querySelector('.add-cc');
-    quantity ++;
-    document.getElementById('qty-cc').textContent = cc
+    cc +=1;
+    document.getElementById('qty-cc').textContent = cc;
+    document.getElementById('qty-total').textContent= gb + cc+ sugar;
+    
+
 })
+
+//code for cc -
+
 document.getElementById('minus-cc').addEventListener('click', function() {
-    console.log('Chocolate Chip + minus button was clicked!')
-    let minusButton =document.querySelector('.minus-cc')
-    if(quantity > 0) {
-        quantity -= 1;}
-        document.getElementById('qty-cc').textContent = cc
+    console.log('Chocolate Chip' + ' '+ 'minus button was clicked!')
+    
+    if(cc > 0) {
+        cc -= 1;
+    }
+        document.getElementById('qty-cc').textContent = cc;
+        document.getElementById('qty-total').textContent= gb + cc+ sugar;
+       
+      
 })
 
 
+//code for sugar +
 
 document.getElementById('add-sugar').addEventListener('click', function(){
-    console.log('Sugar Sprinkle + button was clicked')
+    console.log('Sugar Sprinkle' + ' ' +'button was clicked')
     let plusButton =document.querySelector('.add-sugar');
-    quantity ++;
-    document.getElementById('qty-sugar').textContent = sugar
+    sugar +=1 ;
+    document.getElementById('qty-sugar').textContent = sugar;
+    document.getElementById('qty-total').textContent= gb + cc+ sugar;
+    
+   
 })
+
+//code for sugar -
 document.getElementById('minus-sugar').addEventListener('click', function() {
-    console.log('Sugar Sprinkle + minus button was clicked!')
-    let minusButton =document.querySelector('.minus-sugar')
-    if(quantity > 0) {
-        quantity -= 1;}
-        document.getElementById('qty-sugar').textContent = sugar
+    console.log('Sugar Sprinkle' + ' ' + 'minus button was clicked!')
+  
+    if(sugar > 0) {
+        sugar -= 1;
+    }
+        document.getElementById('qty-sugar').textContent = sugar;
+        document.getElementById('qty-total').textContent= gb + cc+ sugar;
+       
+    
 })
-
-
